0f65bc72bb78999ccce9dde24a0f0e6c12525619
Josef-TL <j.tonnesenlucas@gmail.com>
2024-10-28 09:31:46 +0100

    Refactor database initialization and add occupancy CRUD operations

--- db_service.py
+++ db_service.py
@@ -25,7 +25,31 @@ def init():
         row_count = cur.fetchone()[0]
         
         if row_count == 0:
-            cur.execute(''' INSERT INTO occupancy (RoomId, GuestId, CheckIn, CheckOut) 
-                        VALUES (1, 1, '2021-06-01', '2021-06-03')
+            cur.execute(''' INSERT INTO occupancy (BookingId, RoomId, GuestId, CheckIn, CheckOut) 
+                        VALUES (1, 1, 1, '2021-06-01', '2021-06-03')
                         ''')
     con.commit()
+
+def get_occupancies():
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM occupancy')
+        rows = cur.fetchall()
+
+        occupancies = [{'RowId':row[0], 'BookingId':row[1], 'RoomId':row[2], 'GuestId': row[3], 'CheckIn':row[4], 'CheckOut':row[5]}for row in rows]
+    
+    return occupancies
+
+
+def create_occupancy(occupancy):
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        cur.execute('INSERT INTO occupancy (BookingId, RoomId, GuestId, CheckIn, CheckOut) VALUES (?, ?, ?, ?, ?)',
+                    (occupancy['BookingId'], occupancy['RoomId'], occupancy['GuestId'], occupancy['CheckIn'], occupancy['CheckOut']))
+        
+def update_occupancy(field, row_id):
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        cur.execute('UPDATE occupancy SET CheckOut = ? WHERE RowId = ?', (field['CheckOut'], row_id))
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import db_service, requests
 
 
@@ -7,6 +7,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Occupancy service'
+
+# See all occupancy data
+@app.route('/occupancy')
+def get_occupancy():
+    occupancies = db_service.get_occupancies()
+    return jsonify(occupancies)
+
+# Create a new occupancy
+@app.route('/occupancy', methods=['POST'])
+def create_occupancy():
+    # Checkout is missing, but should still be a key
+    occupancy = request.json
+    db_service.create_occupancy(occupancy)
+    return jsonify(occupancy)
+
+# Update check-out date for an occupancy
+@app.route('/occupancy/<int:row_id>', methods=['PATCH'])
+def update_occupancy(row_id):
+    # In the form of:
+    # {
+    #   "CheckOut": "2021-06-04"
+    # }
+    occupancy = request.json
+    db_service.update_occupancy(occupancy, row_id)
+    return jsonify(occupancy)
+        
+
 
 if __name__ == '__main__':
     db_service.init()  # Ensure the database is initialized before running
